8395b32a443b84dec2194003b838759504f9e60e
Chiyne <74550319+Chiyne@users.noreply.github.com>
2021-04-15 08:52:19 +0700

    Add files via upload
    Классы по БЭМ. 
    Некоторым font-size присвоены переменные.
    Валидация пройдена без ошибок.

--- JS/menu.js
+++ JS/menu.js
@@ -1,9 +1,9 @@
 var hamburger = document.querySelector('.hamburger');
-var menu = document.querySelector('.navigation-list');
+var menu = document.querySelector('.navigation__list');
 
 const toggleMenu = () => {
-    hamburger.classList.toggle('hamburger-active');
-    menu.classList.toggle('navigation-list-active');
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('navigation__list_active');
 }
 
 hamburger.addEventListener('click', e => {
@@ -15,7 +15,7 @@ document.addEventListener('click', e => {
     let target = e.target;
     let its_menu = target == menu;
     let its_hamburger = target == hamburger;
-    let menu_is_active = menu.classList.contains('navigation-list-active');
+    let menu_is_active = menu.classList.contains('navigation__list_active');
 
 
     if (!its_menu && !its_hamburger && menu_is_active) {
